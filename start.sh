2329c77e551f85fc37e2d70404f5ac1a29833cc0
wucc <wuchengcheng@xiaoduotech.com>
2023-09-04 21:49:48 +0800

    e

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ function install_minikube() {
 	# install minikube
 	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 	install minikube-linux-amd64 /usr/local/bin/minikube
-	echo "-------------------------------------------"
+	echo "------------------------------------------- install minikube done!"
 }
 
 install_minikube
@@ -23,21 +23,22 @@ echo "alias k='kubectl'" >> ~/.bashrc
 echo "alias m='minikube'" >> ~/.bashrc
 echo "alias which='command -v'" >> ~/.bashrc
 source /root/.bashrc
-echo "-------------------------------------------"
+echo "------------------------------------------- alias done!"
 
 yum install bash-completion
 kubectl completion bash > /tmp/completion1.sh
 source /tmp/completion1.sh
 kubectl completion bash | sed 's/kubectl/k/g' > /tmp/completion2.sh
 source /tmp/completion2.sh
-echo "-------------------------------------------"
+echo "------------------------------------------- install completion done!"
 
 # minikube start 2 nodes
-minikube start
+minikube start --force
 minikube node add
 minikube node list
+echo "------------------------------------------- minikube start done!"
 
-echo "done!"
+echo "all done!"
 
 
 
